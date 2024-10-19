dd8d0d7cfbce365c198da4eb6a1cd3da74733ab6
YaroslavlPe1 <140802204+YaroslavlPe1@users.noreply.github.com>
2024-10-19 21:55:11 +0300

    Add files via upload

---
+++ Run l vm/scr/Token.h
@@ -0,0 +1,24 @@
+// token.h
+#ifndef TOKEN_H
+#define TOKEN_H
+
+enum class TokenType {
+    NUMBER,
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE,
+    LPAREN,
+    RPAREN,
+    END
+};
+
+struct Token {
+    TokenType type;
+    std::string value;
+
+    Token(TokenType type, const std::string& value) : type(type), value(value) {}
+};
+
+
+#endif // TOKEN_H
